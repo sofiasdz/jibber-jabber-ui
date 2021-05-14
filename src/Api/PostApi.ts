beefa334988e825a4b9ea894daefa08147ad5317
kuhamaven <khalil.stessens@ing.austral.edu.ar>
2021-05-13 22:09:39 -0300

    Implemented all PostAPI methods

--- src/Api/PostApi.ts
+++ src/Api/PostApi.ts
@@ -2,30 +2,27 @@ import {API_BASE_URL} from '../Constants/Constants'
 import {request} from './API'
 import {PostType} from "../components/Types/Types";
 
-export function createPost(title: string, description: string, link: string, topicId: number, role:string): Promise<PostType[]> {
+export function createPost(author: string, body: string): Promise<PostType[]> {
     return request({
-        url: API_BASE_URL + "/post",
+        url: API_BASE_URL + "/posts",
         method: 'POST',
-        body: JSON.stringify({'title': title, 'description': description, 'link': link, 'topicId': topicId, 'role': role}),
-        headers:{'Authorization': 'Bearer '+ localStorage.getItem('token'),'Content-Type': 'application/json'}
+        body: JSON.stringify({'author': author, 'body': body}),
+        headers: {'Authorization': 'Bearer ' + localStorage.getItem('token'), 'Content-Type': 'application/json'}
     });
 }
-
-
 
 export function getPostData(postId: number): Promise<any> {
     return request({
-        url: API_BASE_URL + "/post/" + postId,
+        url: API_BASE_URL + "/posts/" + postId,
         method: 'GET',
         headers: {'Authorization': 'Bearer ' + localStorage.getItem('token'), 'Content-Type': 'application/json'}
     });
 }
 
-export function getPosts(id: number, page: number, size: number): Promise<any> {
+export function getAllPosts(): Promise<any> {
     return request({
-        url: API_BASE_URL + "/topicposts/" + id + '?page=' + page + '&size=' + size,
+        url: API_BASE_URL + "/posts",
         method: 'GET',
         headers: {'Authorization': 'Bearer ' + localStorage.getItem('token'), 'Content-Type': 'application/json'}
     });
 }
-
