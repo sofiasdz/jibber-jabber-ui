9f8c52dfbf9deccf62384fb6d58fbec2b30b5c79
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2021-05-25 11:52:50 -0300

    Implemented react router

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,29 @@ import './App.css';
 import Button from '@material-ui/core/Button';
 import Home from "./components/Home/Home";
 import UserProfile from "./components/UserProfile/UserProfile"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-    <UserProfile></UserProfile>
-    </div>
-  );
+class App extends React.Component  {
+  render() {
+    return (
+        <Router>
+          <div>
+            <h2>Welcome to Jibber Jabber</h2>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <ul className="navbar-nav mr-auto">
+                <li><Link to={'/'} className="nav-link"> Home </Link></li>
+                <li><Link to={'/userprofile'} className="nav-link">UserProfile</Link></li>
+              </ul>
+            </nav>
+            <hr/>
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route path='/userProfile' component={UserProfile}/>
+            </Switch>
+          </div>
+        </Router>
+    );
+  }
 }
 
 export default App;
