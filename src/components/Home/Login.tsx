93aeead042f4b0a1c6e7f92fc25489919f24dd43
khalil <khalil.stessens@ing.austral.edu.ar>
2021-06-25 13:04:32 -0300

    completed proxy, changed api calls accordingly

--- src/components/Home/Login.tsx
+++ src/components/Home/Login.tsx
@@ -1,8 +1,6 @@
 import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import {RouteComponentProps} from 'react-router-dom';
