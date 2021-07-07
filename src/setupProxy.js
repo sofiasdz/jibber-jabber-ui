5d5658e120f0204b694b5090ba51f4736782c481
khalil <khalil.stessens@ing.austral.edu.ar>
2021-07-07 16:05:02 -0300

    Disabled prod proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,44 +1,44 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = app => {
-    app.use(
-        "/posts",
-        createProxyMiddleware ({
-            target: "http://localhost:8001",
-            changeOrigin: true
-        }),
-    );
-    app.use(
-        "/login",
-        createProxyMiddleware ({
-            target: "http://localhost:8002",
-            changeOrigin: true
-        })
-    );
-    app.use(
-        "/users",
-        createProxyMiddleware ({
-            target: "http://localhost:8002",
-            changeOrigin: true
-        })
-    );
-
-    app.use(
-        "/chat",
-        createProxyMiddleware ({
-            target: "http://localhost:8003",
-            changeOrigin: true
-        })
-    );
-
-    app.use(
-        "/conversation",
-        createProxyMiddleware ({
-            target: "http://localhost:8003",
-            changeOrigin: true
-        })
-    );
-
-
+    // app.use(
+    //     "/posts",
+    //     createProxyMiddleware ({
+    //         target: "http://localhost:8001",
+    //         changeOrigin: true
+    //     }),
+    // );
+    // app.use(
+    //     "/login",
+    //     createProxyMiddleware ({
+    //         target: "http://localhost:8002",
+    //         changeOrigin: true
+    //     })
+    // );
+    // app.use(
+    //     "/users",
+    //     createProxyMiddleware ({
+    //         target: "http://localhost:8002",
+    //         changeOrigin: true
+    //     })
+    // );
+    //
+    // app.use(
+    //     "/chat",
+    //     createProxyMiddleware ({
+    //         target: "http://localhost:8003",
+    //         changeOrigin: true
+    //     })
+    // );
+    //
+    // app.use(
+    //     "/conversation",
+    //     createProxyMiddleware ({
+    //         target: "http://localhost:8003",
+    //         changeOrigin: true
+    //     })
+    // );
+    //
+    //
 
 };
